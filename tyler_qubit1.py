46b5858eb3a8c8639880fc1731d85cb74ccd9858
Tyler Schutt <tylerschutt16@gmail.com>
2024-07-01 10:28:17 -0400

    minor effects

--- tyler_qubit1.py
+++ tyler_qubit1.py
@@ -1,7 +1,8 @@
 import numpy as np
 from UnitaryChain import *
+from stringtools import *
 
-def optimize(UC, sub, sigma):
+def rand_optimize(UC, sub, sigma):
     print("subdivisions: {}\tsigma: {}".format(sub, sigma))
     UC.subdivide_at_step(0, sub) # subdividing steps beyond 0 reduces weight by bug
     print(UC.Vs)
@@ -30,6 +31,27 @@ def optimize(UC, sub, sigma):
     print(UC.str())
     print(UC.Vs)
     return UC
+
+def grad_optimize(UC, sub, step, pen):
+	UC.set_coef(penalty=pen)
+	UC.subdivide_at_step(0, sub)
+	print("UC coef: ", UC.coef)
+	grad_desc_step_size = step
+	new_w = UC.weight_total()
+	print("start:   \t{}".format( new_w ))
+	for itr in range(5000):
+		gradH = UC.compute_grad_weight2()
+		old_w = new_w
+		for stp in range(1, UC.N+1):
+			UC.apply_expiH_to_V_at_step(stp, -gradH[stp] * grad_desc_step_size)
+			new_w = UC.weight_total()
+		if np.mod(itr, 50) == 0: print("iter {}:  \t{}".format( itr, new_w ))
+		if new_w > old_w: print("Uh oh...")
+		if new_w + 1e-8 > old_w: break
+
+	print("="*20, "done", "="*20)
+	print("UC coef: ", UC.coef, "\n")
+	print(UC.str())
 
 I2 = np.eye(2, dtype=float)
 PauliX = np.array([[0,1.],[1.,0]])
@@ -86,6 +108,7 @@ for x in range(2, 6):
         minix = x
 print("\n\nThe best approximation is:\n", mini.str(), "\nat {} subdivisions".format(minix))
 """
-optimize(qubit_unitary(Hadamard), 1, 0.05)
+rand_optimize(qubit_unitary(Hadamard), 3, 0.05)
+grad_optimize(qubit_unitary(Hadamard), 3, 0.01, 5.0)
 # UC.subdivide_at_step(0, 2)		## split step 0 into 3 pieces
 # UC.subdivide_at_step(1, 4)		## then, split step 1 into 2 pieces
--- tyler_qubit2.py
+++ tyler_qubit2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from UnitaryChain import *
-
+from stringtools import *
 
 I2 = np.eye(2, dtype=float)
 PauliX = np.array([[0,1.],[1.,0]])
@@ -21,12 +21,15 @@ np.set_printoptions(precision=4, linewidth=10000, suppress=True)
 #UC = two_qubits_unitary(np.kron(I2,PauliY)*1j)		# Rabi 2
 #UC = two_qubits_unitary(np.kron(PauliX,PauliX)*1j)		# conv + gain
 UC = two_qubits_unitary(CntrlZ)
-print(UC.str(), UC.Vs)
+# print(UC.str(), UC.Vs)
 UC2 = [np.kron(Hadamard, Hadamard), np.array([[1/2**(1/2), 0, 0, 1j/2**(1/2)], [0, 1/2**(1/2), 1j/2**(2), 0], [0, 1j/2**(1/2), 1/2**(2), 0], [1j/2**(1/2), 0, 0, 1/2**(1/2)]], dtype=complex), np.kron(Hadamard, Hadamard)]
 
 UC2b = [np.array([[1/2, -1/2, -1/2, 1/2], [1/2, 1/2, -1/2, -1/2], [1/2, -1/2, 1/2, -1/2], [1/2, 1/2, 1/2, 1/2]], dtype=complex), np.array([[1/2**(1/2), 0, 0, 1j/2**(1/2)], [0, 1/2**(1/2), 1j/2**(1/2), 0], [0, 1j/2**(1/2), 1/2**(1/2), 0], [1j/2**(1/2), 0, 0, 1/2**(1/2)]], dtype=complex), np.array([[1/2, 1/2, 1/2, 1/2], [-1/2, 1/2, -1/2, 1/2], [-1/2, -1/2, 1/2, 1/2], [1/2, -1/2, -1/2, 1/2]], dtype=complex)]
 UC.load_U_to_V(UC2b)
-print(UC.str(), UC.Vs)
+print(UC.str())
+# print(joinstr(UC.Vs[1:3]))
+for i in range(len(UC.Vs)):
+	print(to_mathematica_lists(UC.Vs[i]))
 ##	Initialize random number generator
 if np.version.version >= '1.17.0':
 	RNG = np.random.default_rng(55)
@@ -37,21 +40,42 @@ else:
 
 ## Try to update Vs[i] (steps i-1 and i)
 
-"""
+UC = two_qubits_unitary(CntrlZ)
+UC.set_coef(penalty=3.0)
+UC.subdivide_at_step(0, 4)
 UCbk = UC.copy()
-for itr in range(300):
+for itr in range(1):
     for i in range(1, 4):
         old_w = UCbk.weight_total()
-        smallU = random_small_Unitary(4, RNG=RNG, sigma=0.05)
+        smallU = random_small_Unitary(4, RNG=RNG, sigma=5.0)
         UC.check_consistency()
         UC.apply_U_to_V_at_step(i, smallU)
         UC.check_consistency()
         new_w = UC.weight_total()
-        if new_w > old_w:
+        # if new_w > old_w:
         #		print("{} -> {}  (reject)".format( old_w, new_w ))
-            UC = UCbk.copy()
-        else:
+        #    UC = UCbk.copy()
+        # else:
         #		print("{} -> {}  (accept)".format( old_w, new_w ))
-            UCbk = UC.copy()
+        #    UCbk = UC.copy()
 print(UC.str())
-"""
+
+
+print("UC coef: ", UC.coef)
+grad_desc_step_size = 0.0075
+new_w = UC.weight_total()
+print("start:   \t{}".format( new_w ))
+for itr in range(5000):
+	gradH = UC.compute_grad_weight2()
+	old_w = new_w
+	for stp in range(1, UC.N+1):
+		UC.apply_expiH_to_V_at_step(stp, -gradH[stp] * grad_desc_step_size)
+		new_w = UC.weight_total()
+	if np.mod(itr, 50) == 0: print("iter {}:  \t{}".format( itr, new_w ))
+	if new_w > old_w:
+		print("Uh oh...")
+	if new_w + 1e-8 > old_w: break
+
+print("="*20, "done", "="*20)
+print("UC coef: ", UC.coef, "\n")
+print(UC.str())
